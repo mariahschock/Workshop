eee22f359fe9c70f7c28923cbae3fb36a19f42c6
Mariah <mariahschock@gmail.com>
2022-05-24 11:08:32 -0700

    testing

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,33 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { renderWorkshop, renderOption } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderWorkshop', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="workshop"><h2>Defense Against the Dark Arts</h2></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderWorkshop({
+        id: 1,
+        name: 'Defense Against the Dark Arts',
+        participants: [{ name: 'Harry Potter', contact_info: 'hp@hogwarts.com' }],
+    });
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
+});
+
+test('renderOption', (expect) => {
+    const expected = `<option value="1">Defense Against the Dark Arts</option>`;
+    const actual = renderOption({
+        id: 1,
+        name: 'Defense Against the Dark Arts',
+        participants: [{ name: 'Harry Potter', contact_info: 'hp@hogwarts.com' }],
+    });
+    expect.equal(actual.outerHTML, expected);
 });
